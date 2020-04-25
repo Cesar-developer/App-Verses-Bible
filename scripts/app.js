eead91209f516d61bc0f170d395795d71400e4cc
Cesar_developer <kira444.ligh@gmail.com>
2020-04-25 07:38:46 -0600

    Agregado el boton submit al form

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,27 @@
-alert('Hello, how are you?')
+class BibleVerse {
+    constructor(userName, quote, verse) {
+        this.userName = userName;
+        this.quote = quote;
+        this.verse = verse;
+    }
+}
+
+class UI {
+    addBibleVerse() {
+
+    }
+
+    deleteBibleVerse() {
+
+    }
+
+    showMessage() {
+
+    }
+}
+
+// DOM events
+
+document.getElementById('bible-form').addEventListener('submit', function () {
+    alert('Sending form')
+})
